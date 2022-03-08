e91a8533dde6c89b8ee255b5f89073be395ed533
Thiago <tmelo.mat@gmail.com>
2022-03-08 19:38:38 -0300

    test php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+  <h1>test... 123</h1>
+  
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l");
+?> 
+</body>
+</html>
